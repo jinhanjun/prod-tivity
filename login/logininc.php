39bf40ee44f994803b4ca0069829c857a6426cf1
Jason Jin <37589233+jinhanjun@users.noreply.github.com>
2020-11-18 18:54:46 -0500

    Create logininc.php

---
+++ login/logininc.php
@@ -0,0 +1,56 @@
+<?php
+
+if(isset($_POST['loginsubmit'])){
+
+    require "dbhinc.php";
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if(empty($password)){
+        // header("Location: ../signup.php?error=emptyfields");
+        // exit();
+    }
+    else{
+        $sql = "SELECT * FROM users WHERE username=?; ";
+        $stmt = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt, $sql)){
+            header("Location: ../open.php?error=sqlerror");
+            exit();
+        }
+        else {
+
+            mysqli_stmt_bind_param($stmt, "s", $username);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            if($row = mysqli_fetch_assoc($result)){
+                $pwdCheck = password_verify($password, $row['password']);
+                if($pwdCheck == false){
+                    header("Location: ../open.php?error=wrongpassword");
+                    exit();
+                }
+                else if($pwdCheck == true){
+                    session_start();
+                    $_SESSION['userID'] = $row['idusers'];
+                    $_SESSION['userUID'] = $row['username'];
+
+                    header("Location: ../header.php?login=success");
+                    exit();
+
+                }
+                else{
+                    header("Location: ../open.php?error=wrongpassword");
+                    exit();
+                }
+            }
+            else {
+                header("Location: ../open.php?error=nouser");
+                exit();
+            }
+        }
+
+    }
+}
+else {
+    header("Location: ../open.php");
+    exit();
+}
