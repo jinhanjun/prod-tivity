b50a0ac728a3ebe0107cce239d763092c951f42b
Jason Jin <37589233+jinhanjun@users.noreply.github.com>
2020-11-18 19:32:55 -0500

    Update dbhinc.php

--- login/dbhinc.php
+++ login/dbhinc.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "loginsyster";
+$servername = "us-cdbr-east-02.cleardb.com";
+$dBUsername = "bbce67deea96fe";
+$dBPassword = "aa0b3388";
+$dBName = "logindatabase";
 
 $conn = mysqli_connect($servername,$dBUsername, $dBPassword, $dBName );
 if(!$conn){
