eea20d2c756a6b32964a9570b2b0a424f835ce1b
winren99 <46582775+winren99@users.noreply.github.com>
2020-11-20 23:34:35 -0500

    Update signup.php
    Update look

--- signup.php
+++ signup.php
@@ -40,15 +40,12 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-    />
-    <link
-      href="https://fonts.googleapis.com/css?family=Roboto:400,700"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="signup.css"/>
+
     <script src="board.js"></script>
     <script type = "module" src="app.js"></script>
     <script src="https://www.google.com/recaptcha/api.js"></script>
@@ -56,58 +53,90 @@
 
 
     <title>Productivity Board</title>
+
   </head>
+
+
   <body>
     <div class="ui">
       <nav>
         <div class="nav--left">
-          <a style="color:white;"> Welcome to your personalized board!</a>
+          <a style="color:white;"> Welcome to Prod-tivity!</a>
 
         </div>
 
-        <div class="nav--right">
+        <!-- <div class="nav--right">
           <a href="open.php" name="logout" style="color:white;">Login</a>
-        </div>
-      </nav>
-      <header><h1>SIGN UP</h1></header>
-      <?php
-        if(isset($_GET['error'])){
-            if($_GET['error']== "usertaken"){
-                echo '<h1>User already exists.</h1>';
+        </div> -->
+      </nav>  
+      <br /> <br /> 
+         
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <div id = "signupcard" class = "card rounded shadow shadow-sm"> 
+            <div class="card-header">
+              <h3 class="mb-0">Sign Up Here</h3>
+            </div>
 
-            } else if($_GET['error']== "emptyfields"){
-                echo '<h1>A field was left empty.</h1>';
+            <div class="card-body">
+              <form id = "submitform" action="login/signupinc.php" method="post">
+                  <label for="signupub">Username</label>
+                  <input id = "signupub" class="form-control form-control-lg form-rounded" name="username" type = "text" placeholder="Create a username" >
+                  
+                  <br>
 
-            }else if($_GET['error']== "sqlerror"){
-                echo '<h1>Database error</h1>';
+                  <label for="signuppass">Password</label>
+                  <input id = "signuppass" class="form-control form-control-lg form-rounded" name="password" type = "text" placeholder="Create your password" >
+                      
+                  <br /> 
+                  <label for="signuppass2">Re-enter Your Password</label>
+                  <input id = "signuppass2" class="form-control form-control-lg form-rounded" name="password2" type = "text" placeholder="Repeat password" >
 
-            }
-            echo '<h1> Please try again</h1>';
-        } else if (isset($_GET['signup'])== "success") {
-            echo '<h1>SUCCESS!</h1>';
-            echo '<a href="header.php"><button style="width: 10%; background-color: #0067a3; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px;"type="submit" >Enter</button></a>';
+                  <br />
+                  <button id = "submitsignup" class="btn btn-success btn-lg float-left" type="submit" name="signupsubmit">Signup</button>
+              </form>
+            
+              <div class = "float-right">
+              
+              <?php
+                if(isset($_GET['error'])){
+                    if($_GET['error']== "usertaken"){
+                        echo '<p class = "superror">Username is already taken. Please try again.</p>';
 
-        }
-      ?>
-      <br /> <br />
-      <form id = "submitform" action="login/signupinc.php" method="post" >
-                        <input style="width: 25%; padding: 12px 20px; margin: 8px 0; box-sizing: border-box;" name="username" type = "text" placeholder="Username" >
-                        <input style="width: 25%; padding: 12px 20px; margin: 8px 0; box-sizing: border-box;" name="password" type = "text" placeholder="Password" >
-                        <input style="width: 25%; padding: 12px 20px; margin: 8px 0; box-sizing: border-box;" name="password2" type = "text" placeholder="Repeat Password" >
+                    } else if($_GET['error']== "emptyfields"){
+                        echo '<p class = "superror">A field was left empty. Please try again.</p>';
 
-                        <br /> <br />
-                        <button style="width: 10%; background-color: #0067a3; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px;"type="submit" name="signupsubmit">Signup</button>
-                        </form>
-                        <br /> <br />
-                        <div class="field">
-                            <div class="control">
-                                
-                            </div>
+                    }else if($_GET['error']== "sqlerror"){
+                        echo '<p class = "superror">Database error. Please try again.</p>';
+                    }
+                } else if (isset($_GET['signup'])== "success") {
+                  echo '<p style = "color: green;"> Success!  Return to Login to sign in. </p> ';
+                  echo '<a href="header.php"><button class="btn btn-primary btn-lg float-right" type="submit" >Enter</button></a>';
 
-                        </div>
+                }
+              ?> 
+              
+              </div>
+            </div>
+
+
+          <br />
+
+          <div class = "card-footer">
+          <p>Already Have an Account? Return back to  <a id = "loginlink" href="open.php" name="logout" >Login.</a></p>
+          </div>
+
+          <div class="field">
+              <div class="control">                
+              </div>
+          </div>
         
+        </div>
+      </div>
     </div>
-    <script>
+    
+
+
+  <script>
   grecaptcha.ready(function() {
       grecaptcha.execute('6LfbkuQZAAAAAEYx3CAJz2dgr7m8LT_2h-CacyuY', {action: 'homepage'}).then(function(token) {
          // console.log(token);
@@ -124,3 +153,4 @@
      document.getElementById("demo-form").submit();
    }
  </script>
+
