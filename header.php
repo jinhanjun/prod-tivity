81f8e5e1666ee01b370a2022ef3be65e0b797061
Jason Jin <37589233+jinhanjun@users.noreply.github.com>
2020-11-21 19:14:49 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -12,42 +13,129 @@
       href="https://fonts.googleapis.com/css?family=Roboto:400,700"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="style.css" />
-    <script src="board.js"></script>
-    <script type = "module" src="app.js"></script>
-    <title>Productivity Board</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style type = "text/css">
+    body {
+    background: #1f6096;
+    }
+
+    .container {
+      background: #1f6096;
+      min-height: 100%;
+      height: auto !important;
+      height: 100%;
+      margin: 0 auto -20px;
+      padding: 20px;
+    }
+    
+    .card-deck .card {
+      max-width: 99%;
+    } 
+    </style>
+
+    <title>Busy Bee</title>
   </head>
+
   <body>
-    <div class="ui">
-      <nav>
-        <div class="nav--left">
-          <a href="#"> Boards</a>
-          <a href="#">Contributors</a>
-        </div>
-        <div class="nav--right">
-        <?php
-        if(isset($_SESSION['username'])){
-            echo '<a href="open.php" name="logout">Login</a>';
-        }
-        else {
-            echo '<a href="login/logoutinc.php" name="logout">Logout</a>';;
-
-        }
-        ?>
-          
-        </div>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <ul class="navbar-nav">
+          <h3 style = "color: goldenrod;">Welcome to your Busy Bee Board!</h3>
+          <li class="nav-item">
+            <a class="nav-link " href="login/logoutinc.php" name = "logout">Logout</a>
+          </li>
+        </ul>
       </nav>
-      <header><h1>Welcome to your board!</h1></header>
-      <br /> <br />
-      <div class="container">
 
-      </div>  
+
+      <!-- this is the container -->
+      <div class="container card-deck">
+
+        <div class="col-sm-3 col-md-3 pb-2">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <label for = "new-todo">Todo:</label>
+              <textarea id = "new-todo" rows = "3" class="form-control form-rounded" name="newtodo" type = "text" placeholder="Type in a task." contentEditable="true"></textarea>
+                <br>
+              <button type="submit" class="btn btn-primary float-right">Add</button>
+            </div>
+          </div>
+        </div>
+
+      <?php
+      session_start();
+      $username = $_SESSION['userUID'];
+
+            $servername = "us-cdbr-east-02.cleardb.com";
+            $dBUsername = "bbce67deea96fe";
+            $dBPassword = "aa0b3388";
+            $dBName = "heroku_594dc794615938b";
+
+            // Create connection
+            $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+            // Check connection
+            if(!$conn){
+                die("Connection failed: " .mysqli_connect_err());
+            }
+
+    // $sql = "SELECT cardtext FROM prodcards WHERE username='test';";  //This is where I specify what data to query
+    // $result = $conn->query($sql);
+    // echo json_encode($result);
+
+    // $mysqli = new mysqli($servername, $dBUsername, $dBPassword, $dBName);
+    // $result = $mysqli->query("SELECT cardtext FROM prodcards WHERE username='test';");
+    // $rows = $result->fetch_assoc();
+    // //echo htmlentities($row['_message']);
+    // foreach($rows as $row){
+    //     echo htmlentities($row);
         
-    </div>
-  </body>
-</html>
+    // }
 
-<script>
-  alert(document.getElementById("temporaryusername").value )
-  document.getElementById("temporaryusername").value = document.getElementById("username").value;
-</script>
+    //$username = $_SESSION['username'];
+    $q = "SELECT cardtext as _message FROM prodcards WHERE username='$username';";
+    $r = mysqli_query($conn, $q);
+
+
+    function writeNot($text) {
+        return '<div class="col-sm-3 col-md-3 pb-2">
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+        <p class="card-text">(\"$text\")</p>
+        
+        <a href="#" class="">Delete</a><
+        /div></div></div>';
+    }
+
+    while($page = mysqli_fetch_assoc($r)){
+
+
+        echo '<div class="col-sm-3 col-md-3 pb-2">
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+        <p class="card-text">';
+        
+        echo $page["_message"];
+        
+        echo '</p>
+        <a href="#" class="float-right">Update</a>
+        <a href="#" class="">Delete</a></div></div></div>';
+    }
+    ?>
+
+
+        <!-- THIS IS THE CODE FOR THE CARD,,,, put this in the while loop
+        <div class="col-sm-3 col-md-3 pb-2">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="">Delete</a>
+            </div>
+          </div>
+        </div>
+        ENDING DIV TAG FOR CARD ^^^ -->
+
+
+        <!-- div tag below is for the container of cards  -->
+      </div>
+   
+</body>
+</html>
