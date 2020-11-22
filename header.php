f0b340dd920ae5f29171fa6ee0864f91c18c8885
winren99 <46582775+winren99@users.noreply.github.com>
2020-11-22 16:30:48 -0500

    Update header.php
    hope this works

--- header.php
+++ header.php
@@ -1,229 +1,207 @@
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-    />
-    <link
-      href="https://fonts.googleapis.com/css?family=Roboto:400,700"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <style type = "text/css">
-    body {
-    background: #1f6096;
-    }
 
-    .container {
-      background: #1f6096;
-      min-height: 100%;
-      height: auto !important;
-      height: 100%;
-      margin: 0 auto -20px;
-      padding: 20px;
-    }
-    
-    .card-deck .card form{
-      max-width: 99%;
-      padding: 20px;
-    } 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style type="text/css">
+        body {
+            background: #463b93;
+        }
+
+        .container {
+            background: #463b93;
+            min-height: 100%;
+            height: auto !important;
+            height: 100%;
+            margin: 0 auto -20px;
+            padding: 20px;
+        }
+
+        .card-deck .card form {
+            max-width: 100%;
+            /* padding: 20px; */
+        }
     </style>
 
     <title>Busy Bee</title>
-  </head>
+</head>
 
-  <body>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <ul class="navbar-nav">
-          <h3 style = "color: goldenrod;">Welcome to your Busy Bee Board!</h3>
-          <li class="nav-item">
-            <a class="nav-link " href="login/logoutinc.php" name = "logout">Logout</a>
-          </li>
-        </ul>
-      </nav>
+<body>
+    <div class="navbar shadow-lg " style="background-color: #262a2b ">
+        <a class="navbar-brand" style=" color: white;">
+            <img src="https://img.icons8.com/nolan/64/bee.png" />
+            <strong> Busy Bee </strong>
+        </a>
 
-<?php
-//REST API START
-function call_api($method, $url, $data = false,$api_key="_9A3UF457b8_wDR2Vr7W_QeF")
-{
-    $curl = curl_init();
+        <a style = "color: #808184;" class="nav-link " href="login/logoutinc.php" name="logout">Logout</a>
 
-    switch ($method)
+    </div>
+
+    <?php
+    //REST API START
+    function call_api($method, $url, $data = false,$api_key="_9A3UF457b8_wDR2Vr7W_QeF")
     {
-	case "POST":
-	    curl_setopt($curl, CURLOPT_POST, 1);
+        $curl = curl_init();
 
-	    if ($data)
-		curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-	    break;
-	case "PUT":
-	    curl_setopt($curl, CURLOPT_PUT, 1);
-	    break;
-	default:
-	    if ($data)
-		$url = sprintf("%s?%s", $url, http_build_query($data));
+        switch ($method)
+        {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+
+            if ($data)
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_PUT, 1);
+            break;
+        default:
+            if ($data)
+            $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
+
+        $headers = [
+        'Content-Type: application/json'
+        ];
+        if ( !empty($api_key))
+        $headers[] = 'X-TheySaidSo-Api-Secret: '. $api_key;
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $result;
     }
 
-    $headers = [
-	'Content-Type: application/json'
-	];
-    if ( !empty($api_key))
-	$headers[] = 'X-TheySaidSo-Api-Secret: '. $api_key;
+    $qod_result = call_api("GET","https://quotes.rest/qod?category=inspire",false,"_9A3UF457b8_wDR2Vr7W_QeF");
+    $encode = json_decode($qod_result, true);
+    $quote = $encode;
 
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    //DISPLAY QUOTE AND AUTHOR
 
-    $result = curl_exec($curl);
+    // echo '<ul>';
+    // // echo $qod_result;
+    // echo $quote['contents']['quotes'][0]['quote'];
+    // echo ' Author: ';
+    // echo $quote['contents']['quotes'][0]['author'];
+    // echo '</ul>';
 
-    curl_close($curl);
+    //REST API END
+    ?>
 
-    return $result;
-}
+    <!-- this is the container -->
+    <div class="container card-deck">
 
-$qod_result = call_api("GET","https://quotes.rest/qod?category=inspire",false,"_9A3UF457b8_wDR2Vr7W_QeF");
-$encode = json_decode($qod_result, true);
-$quote = $encode;
-
-//DISPLAY QUOTE AND AUTHOR
-
-echo '<ul>';
-echo $qod_result;
-echo $quote['contents']['quotes'][0]['quote'];
-echo ' Author: ';
-echo $quote['contents']['quotes'][0]['author'];
-echo '</ul>';
-
-//REST API END
-?>
-
-
-      <!-- this is the container -->
-      <div class="container card-deck">
-      <form class = "cardform"id = "submitform" action="postcard.php" method="post">
-        <div class="col-sm-3 col-md-3 pb-2">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <label for = "new-todo">Todo:</label>
-              <textarea id = "new-todo" rows = "3" class="form-control form-rounded" name="new-todo" type = "text" placeholder="Type in a task." contentEditable="true"></textarea>
-                <br>
-              <button type="submit" name = "addsubmit" class="btn btn-primary float-right">Add</button>
-              <?php
-            //   session_start();
-            //   $servername = "us-cdbr-east-02.cleardb.com";
-            //   $dBUsername = "bbce67deea96fe";
-            //   $dBPassword = "aa0b3388";
-            //   $dBName = "heroku_594dc794615938b";
-            //   $username = $_SESSION['userUID'];
-  
-            //   // Create connection
-            //   $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
-            //   // Check connection
-            //   if(!$conn){
-            //       die("Connection failed: " .mysqli_connect_err());
-            //   }
-           
-            //   if( isset($_POST['addsubmit'])){
-            //         $newtodo = $_POST['new-todo'];
-            //         $sql = "INSERT INTO prodcards (username, text) VALUES ('$username', '$newtodo')";
-            //         mysqli_query($conn, $sql);
-            //         mysqli_close($conn);
-
-            //         echo '</div></div></div><div class="col-sm-3 col-md-3 pb-2">
-            //         <div class="card" style="width: 18rem;">
-            //         <div class="card-body">
-            //         <p class="card-text">';
-                    
-            //         echo $newtodo["_message"];
-                    
-            //         echo '</p>
-            //         <a href="#" class="float-right">Update</a>
-            //         <a href="#" class="">Delete</a></div></div></div>';
-            //   }
-        
-            ?>
+        <form class="cardform" id="submitform" action="postcard.php" method="post">
+            <div class="col-sm-3 col-md-3 pb-2">
+                <div class="card" style="width: 32rem;">
+                    <div class="card-body">
+                        <label for="new-todo">Todo:</label>
+                        <textarea id="new-todo" rows="3" class="form-control form-rounded" name="new-todo" type="text"
+                            placeholder="Type in your task." contentEditable="true"></textarea>
+                        <br>
+                        <button type="submit" name="addsubmit" class="btn btn-warning float-right ">+ Add</button>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
         </form>
-      <?php
-      session_start();
-      $username = $_SESSION['userUID'];
 
-      $servername = "aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-      $dBUsername = "criar4b0z3v8vsbh";
-      $dBPassword = "mcjw7gey3e0sbow8";
-      $dBName = "k1d10389nz4l3bsk";
+        <div class="col-sm-3 col-md-3 pb-2">
+                <div class="card" style="width: 30.9rem; height: 14rem; background-color: #aab9fe;">
+                    <div class="card-header">
+                    <h4>Inspirational Quote of the Day</h4>
+                    </div>
+                    <div class="card-body">
+                    
+                    <?php 
+                    echo '<ul><h6>"';
+                    echo $quote['contents']['quotes'][0]['quote'];
+                    echo '"</h6>';
+                    echo ' ––– ';
+                    echo $quote['contents']['quotes'][0]['author'];
+                    echo '</ul>';
+                    ?>
 
-            // Create connection
-            $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
-            // Check connection
-            if(!$conn){
-                die("Connection failed: " .mysqli_connect_err());
-            }
-
-    // $sql = "SELECT cardtext FROM prodcards WHERE username='test';";  //This is where I specify what data to query
-    // $result = $conn->query($sql);
-    // echo json_encode($result);
-
-    // $mysqli = new mysqli($servername, $dBUsername, $dBPassword, $dBName);
-    // $result = $mysqli->query("SELECT cardtext FROM prodcards WHERE username='test';");
-    // $rows = $result->fetch_assoc();
-    // //echo htmlentities($row['_message']);
-    // foreach($rows as $row){
-    //     echo htmlentities($row);
+                    <!-- echo '<ul>';
+                    echo 'Vision without action is daydream. Action without vision is nightmare..';
+                    echo '<br>';
+                    echo ' ––– ';
+                    echo 'Japanese Proverb';
+                    echo '</ul>';
+                     -->
+                   
+                    </div>
+                </div>
+        </div>
         
-    // }
+        <?php
+          session_start();
+          $username = $_SESSION['userUID'];
 
-    //$username = $_SESSION['username'];
-    $q = "SELECT cardtext as _message, cardid as _id, cardtext as _text FROM prodcards WHERE username='$username';";
-    $r = mysqli_query($conn, $q);
+          $servername = "aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+          $dBUsername = "criar4b0z3v8vsbh";
+          $dBPassword = "mcjw7gey3e0sbow8";
+          $dBName = "k1d10389nz4l3bsk";
+
+                // Create connection
+                $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+                // Check connection
+                if(!$conn){
+                    die("Connection failed: " .mysqli_connect_err());
+                }
+
+          $q = "SELECT cardtext as _message, cardid as _id, cardtext as _text FROM prodcards WHERE username='$username';";
+          $r = mysqli_query($conn, $q);
 
 
 
     while($page = mysqli_fetch_assoc($r)){ ?>
 
         <div class="col-sm-3 col-md-3 pb-2">
-        <div class="card" style="width: 18rem;">
-        <div id=<?php echo $page["_id"]?> class="card-body">
-        <p class="card-text">
-        
-        <?php echo $page["_message"] ?>
-        
-        </p>
-        <td><a href="header.php?edit=true&id=<?php echo $page["_id"]?>">Edit</a></td>
+            <div class="card" style="width: 15rem; background-color: white;">
+                <div id=<?php echo $page["_id"]?> class="card-body">
+                    <p class="card-text">
 
+                        <?php echo $page["_message"] ?>
 
-            <?php
+                    </p>
+                    <td><a class="btn btn-outline-success float-left" href="header.php?edit=true&id=<?php echo $page["_id"]?>">Edit</a></td>
+
+                    <?php
 
 
                 if(isset($_GET['edit']) && ($_GET['id']== $page["_id"])){?>
-                      <form class = "cardform"id = "submitform" action="editcard.php?id=<?php echo $page["_id"]?>" method="post">
-                    <textarea id = "new-todo" rows = "3" class="form-control form-rounded" name="new-todo" type = "text" placeholder="<?php echo $page["_text"]?>" contentEditable="true"></textarea>
-                    <br>
-                  <button type ="submit" href=""class="btn btn-primary float-right" >Add</button></form>
-                <?php } ?>
-        <a href="deletecard.php?id=<?php echo $page["_id"]?>">Delete</a></div></div></div>
-   
-    <?php } ?>    
-    
-    <script>
-        renderedit($editid){
-            <?php
-            //echo('<textarea id = "new-todo" rows = "3" class="form-control form-rounded" name="new-todo" type = "text" placeholder="Type in a task." contentEditable="true"></textarea><br><button type="submit" name = "addsubmit" class="btn btn-primary float-right">Add</button>')
-            ?>
+                    <form class="cardform" id="submitform" action="editcard.php?id=<?php echo $page["_id"]?>"
+                        method="post">
+                        <br>
+                        <textarea id="new-todo" rows="3" class="form-control form-rounded" name="new-todo" type="text"
+                             contentEditable="true"><?php echo $page["_text"]?></textarea>
+                        <br>
+                        <button class = "btn btn-primary float-left" type="submit" href="">Update</button></form>
+                    <?php } ?>
+                    <a class="btn btn-danger float-right" href="deletecard.php?id=<?php echo $page["_id"]?>">Delete</a>
+
+                    <!-- <button type="submit" name="addsubmit" class="btn btn-warning float-right ">+ Add</button> -->
+                </div>
+            </div>
+        </div>
+
+        <?php } ?>
+
+        <script>
+            renderedit($editid){
         }
 
-    </script>
-       
-
-
-
+        </script>
 
         <!-- THIS IS THE CODE FOR THE CARD,,,, put this in the while loop
         <div class="col-sm-3 col-md-3 pb-2">
@@ -238,7 +216,16 @@ echo '</ul>';
 
 
         <!-- div tag below is for the container of cards  -->
-      </div>
-   
+    </div>
+
+    <footer style="      
+    background-color: #262a2b;" class="footer text-center py-3 page-footer fixed-bottom">
+
+        <a style="text-decoration: none; color: white;" href="https://icons8.com/icon/vn5qFmwrStFg/bee">Bee icon by
+            Icons8</a>
+
+    </footer>
+
 </body>
+
 </html>
